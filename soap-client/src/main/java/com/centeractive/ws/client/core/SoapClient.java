f3b3e86abd30fc0ba50aef7441867f15dacf5efc
Tom Bujok <tom.bujok@reficio.org>
2012-09-19 14:27:48 +0200

    Handling soap-action properly - 2 guess

--- soap-client/src/main/java/com/centeractive/ws/client/core/SoapClient.java
+++ soap-client/src/main/java/com/centeractive/ws/client/core/SoapClient.java
@@ -171,9 +171,8 @@ public final class SoapClient {
 
     private void decorateConnectionWithSoap(String soapAction, String requestEnvelope) {
         if (requestEnvelope.contains(SOAP_1_1_NAMESPACE)) {
-//            if (soapAction != null) {
-                connection.setRequestProperty(PROP_SOAP_ACTION_11, soapAction);
-//            }
+            soapAction = soapAction != null ? "\"" + soapAction + "\"" : "";
+            connection.setRequestProperty(PROP_SOAP_ACTION_11, soapAction);
             connection.setRequestProperty(PROP_CONTENT_TYPE, MIMETYPE_TEXT_XML);
         } else if (requestEnvelope.contains(SOAP_1_2_NAMESPACE)) {
             connection.setRequestProperty(PROP_CONTENT_TYPE, MIMETYPE_APPLICATION_XML);
